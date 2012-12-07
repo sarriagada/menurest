3d147df6f828deb3516a7fac0166cf748c816e63
Paula Lencina Estévez <paula.le@live.com.ar>
2012-12-07 00:30:37 -0200

    [Modificación de diseño: Form Login]
      - Se agregó el diseño gráfico del form Login.
      - Además se realizó un pequeño efecto visual al pasar por el botón
        ENTRAR.

--- menurest/Form1.Designer.vb
+++ menurest/Form1.Designer.vb
@@ -19,62 +19,91 @@ Partial Class frm_login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_login))
         Me.txt_nombre = New System.Windows.Forms.TextBox
         Me.txt_pass = New System.Windows.Forms.TextBox
-        Me.lbl_registro = New System.Windows.Forms.Label
-        Me.lbl_entrar = New System.Windows.Forms.Label
+        Me.pic_entrar = New System.Windows.Forms.PictureBox
+        Me.pic_registrarse = New System.Windows.Forms.PictureBox
+        Me.pic_entrar_on = New System.Windows.Forms.PictureBox
+        CType(Me.pic_entrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_registrarse, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic_entrar_on, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txt_nombre
         '
-        Me.txt_nombre.Location = New System.Drawing.Point(79, 62)
+        Me.txt_nombre.Location = New System.Drawing.Point(74, 100)
+        Me.txt_nombre.Margin = New System.Windows.Forms.Padding(2)
         Me.txt_nombre.Name = "txt_nombre"
-        Me.txt_nombre.Size = New System.Drawing.Size(100, 22)
+        Me.txt_nombre.Size = New System.Drawing.Size(169, 20)
         Me.txt_nombre.TabIndex = 0
         '
         'txt_pass
         '
-        Me.txt_pass.Location = New System.Drawing.Point(79, 109)
+        Me.txt_pass.Location = New System.Drawing.Point(74, 139)
+        Me.txt_pass.Margin = New System.Windows.Forms.Padding(2)
         Me.txt_pass.Name = "txt_pass"
-        Me.txt_pass.Size = New System.Drawing.Size(100, 22)
+        Me.txt_pass.Size = New System.Drawing.Size(169, 20)
         Me.txt_pass.TabIndex = 1
         '
-        'lbl_registro
+        'pic_entrar
         '
-        Me.lbl_registro.AutoSize = True
-        Me.lbl_registro.Location = New System.Drawing.Point(44, 177)
-        Me.lbl_registro.Name = "lbl_registro"
-        Me.lbl_registro.Size = New System.Drawing.Size(81, 17)
-        Me.lbl_registro.TabIndex = 2
-        Me.lbl_registro.Text = "Registrarse"
+        Me.pic_entrar.Image = CType(resources.GetObject("pic_entrar.Image"), System.Drawing.Image)
+        Me.pic_entrar.Location = New System.Drawing.Point(83, 164)
+        Me.pic_entrar.Name = "pic_entrar"
+        Me.pic_entrar.Size = New System.Drawing.Size(138, 66)
+        Me.pic_entrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pic_entrar.TabIndex = 3
+        Me.pic_entrar.TabStop = False
         '
-        'lbl_entrar
+        'pic_registrarse
         '
-        Me.lbl_entrar.AutoSize = True
-        Me.lbl_entrar.Location = New System.Drawing.Point(168, 177)
-        Me.lbl_entrar.Name = "lbl_entrar"
-        Me.lbl_entrar.Size = New System.Drawing.Size(47, 17)
-        Me.lbl_entrar.TabIndex = 3
-        Me.lbl_entrar.Text = "Entrar"
+        Me.pic_registrarse.Image = CType(resources.GetObject("pic_registrarse.Image"), System.Drawing.Image)
+        Me.pic_registrarse.Location = New System.Drawing.Point(176, 240)
+        Me.pic_registrarse.Name = "pic_registrarse"
+        Me.pic_registrarse.Size = New System.Drawing.Size(109, 14)
+        Me.pic_registrarse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pic_registrarse.TabIndex = 4
+        Me.pic_registrarse.TabStop = False
+        '
+        'pic_entrar_on
+        '
+        Me.pic_entrar_on.Image = CType(resources.GetObject("pic_entrar_on.Image"), System.Drawing.Image)
+        Me.pic_entrar_on.Location = New System.Drawing.Point(83, 164)
+        Me.pic_entrar_on.Name = "pic_entrar_on"
+        Me.pic_entrar_on.Size = New System.Drawing.Size(138, 66)
+        Me.pic_entrar_on.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pic_entrar_on.TabIndex = 5
+        Me.pic_entrar_on.TabStop = False
+        Me.pic_entrar_on.Visible = False
         '
         'frm_login
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(282, 255)
-        Me.Controls.Add(Me.lbl_entrar)
-        Me.Controls.Add(Me.lbl_registro)
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(297, 266)
+        Me.Controls.Add(Me.pic_entrar_on)
+        Me.Controls.Add(Me.pic_registrarse)
+        Me.Controls.Add(Me.pic_entrar)
         Me.Controls.Add(Me.txt_pass)
         Me.Controls.Add(Me.txt_nombre)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frm_login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "menurest"
+        CType(Me.pic_entrar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_registrarse, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic_entrar_on, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents txt_nombre As System.Windows.Forms.TextBox
     Friend WithEvents txt_pass As System.Windows.Forms.TextBox
-    Friend WithEvents lbl_registro As System.Windows.Forms.Label
-    Friend WithEvents lbl_entrar As System.Windows.Forms.Label
+    Friend WithEvents pic_entrar As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_registrarse As System.Windows.Forms.PictureBox
+    Friend WithEvents pic_entrar_on As System.Windows.Forms.PictureBox
 
 End Class
--- menurest/Form1.vb
+++ menurest/Form1.vb
@@ -1,3 +1,14 @@
 Public Class frm_login
-
+    '************************************************
+    'Efecto al pasar el mouse por el botón ENTRAR.
+    'Se oculta una imagen y se muestra otra para simularlo.
+    Private Sub pic_entrar_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_entrar.MouseEnter
+        pic_entrar.Visible = False
+        pic_entrar_on.Visible = True
+    End Sub
+    Private Sub pic_entrar_on_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles pic_entrar_on.MouseLeave
+        pic_entrar_on.Visible = False
+        pic_entrar.Visible = True
+    End Sub
+    '************************************************
 End Class
